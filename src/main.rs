a84390b933111477ec2fd891d25305424f6a7b17
dom <dom@dwlabs.co.uk>
2025-06-20 13:33:07 +0100

    feat: check VTOR

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::asm::nop;
 // pick a panicking behavior
 use panic_halt as _;
 
@@ -12,6 +13,17 @@ fn main() -> ! {
     let p = unsafe { ra4m1::Peripherals::steal() };
     // Set p111 as an output
     p.PORT1.pdr().write(|w| unsafe { w.bits(1 << 11) });
+
+    // check VTOR register
+    const VTOR_ADDRESS: *const u32 = 0xE000ED08 as *const u32;
+    let value = unsafe { core::ptr::read_volatile(VTOR_ADDRESS) };
+    if value != 0x4000 {
+        p.PORT1.podr().write(|w| unsafe { w.bits(1 << 11) });
+        loop {
+            nop();
+        }
+    }
+
     loop {
         // Set output high
         p.PORT1.podr().write(|w| unsafe { w.bits(1 << 11) });
